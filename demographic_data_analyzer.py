b3efa794ff33d334c70a06b91236e03da4211e72
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-08 11:23:06 -0800

    passed race test;

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv', index_col=0)
+    
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df.groupby('race').size()
 
     # What is the average age of men?
     average_age_men = None
